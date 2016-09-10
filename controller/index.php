1d977609719ce1b87dacde8a85b24e8945139661
narender <kumar.narender927@gmail.com>
2016-09-10 18:57:49 +0530

    dddd

---
+++ controller/index.php
@@ -0,0 +1,25 @@
+<?php
+echo "user registerd";                           
+/*  include_once("../model/Model.php"); 
+
+$model = new Model();
+
+ if (isset($_POST['name'])&&isset($_POST['password'])&&isset($_POST['email'])){
+			$name = $_POST['name'];	
+			$password= $_POST['password'];	
+			$email = $_POST['email'];
+			$country = $_POST['country'];
+			if(empty($name)||empty($password)||empty($email)||empty($country)){
+		   header("location:http://localhost/login/view/regform.php?id=empty");
+		    }
+		else{
+	
+			$model->insert($name,$password,$email,$country);
+			header("location:http://localhost/login/index.php?id=added");
+}
+ } */
+	
+
+
+
+?>
