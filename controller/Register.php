f126b7bff08af8d3b031531929247dd07a2a004d
narender <kumar.narender927@gmail.com>
2016-09-12 18:04:45 +0530

    ddd

--- controller/Register.php
+++ controller/Register.php
@@ -2,9 +2,9 @@
 include_once("../model/Model.php"); 
 
 $model = new Model();
-   PRINT_R($_POST);
-			exit;
- if (isset($_POST['name'])&&isset($_POST['password'])&&isset($_POST['email'])){
+PRINT_R($_POST);
+exit;
+ /* if (isset($_POST['name'])&&isset($_POST['password'])&&isset($_POST['email'])){
 			$name = $_POST['name'];	
 			$password= $_POST['password'];	
 			$email = $_POST['email'];
@@ -16,12 +16,12 @@ $model = new Model();
 					//header("location:http://localhost/view/regform.php?id=empty");
 		    }
 		else{
-	
+	          
 			$model->insert($name,$password,$email,$country);
 			header("Location:https://".$_SERVER['SERVER_NAME'].'/index.php?id=added');
 			//header("location:http://localhost/login/index.php?id=added");
+   }
 }
- }
 	
 
 
