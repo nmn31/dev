5e89df4c9d1b77dd1787ae0b677c9ed664a13e1f
narender <kumar.narender927@gmail.com>
2016-09-12 18:36:33 +0530

    dddddddd

--- controller/Register.php
+++ controller/Register.php
@@ -1,17 +1,18 @@
 <?php 
  $include = "https://".$_SERVER['SERVER_NAME'].'/model/Model.php';
  print_r($include);
- exit;
  include_once($include); 
 
             $model = new Model(); 
+			print_r($model)
+			exit;
             //PRINT_R($_POST);
 			$name = $_POST['name'];	
 			$password= $_POST['password'];	
 			$email = $_POST['email'];
 			$country = $_POST['country'];
 			echo "<pre>";
-			print_r($model);
+			;
 			
 			if(empty($name)||empty($password)||empty($email)||empty($country)){
 					header("Location:https://".$_SERVER['SERVER_NAME'].'/view/regform.php?id=empty');
