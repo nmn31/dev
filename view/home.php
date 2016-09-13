276a11e22d1b4472aa87b579a3eea96e0194625d
narender <kumar.narender927@gmail.com>
2016-09-13 12:53:16 +0530

    login

--- view/home.php
+++ view/home.php
@@ -1,10 +1,10 @@
 <?php
 
     session_start();
-    include_once("../controller/Session.php"); 
+    /* include_once("../controller/Session.php"); 
 	include_once("../controller/Fetchcontroller.php"); 
     include_once("../model/Fetch.php");
-    include_once("../view/header.php");
+    include_once("../view/header.php"); */
 	
 print_r($_SESSION);
 ?>
