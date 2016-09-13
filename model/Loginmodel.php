a9ba3362d780c718a3b22418075a1a083e254643
narender <kumar.narender927@gmail.com>
2016-09-13 10:50:40 +0530

    dd

--- model/Loginmodel.php
+++ model/Loginmodel.php
@@ -1,13 +1,14 @@
 <?php
+echo "sdfahosdfafsdfsafdsaadsfadfsdafs";
 $servername = "sql6.freemysqlhosting.net";
 $username = "sql6135475";
 $password = "8zKGpAzm6J";
-    $link = @mysql_connect($servername, $username, $password);
-    mysql_select_db("sql6135475", $link);
+$link = @mysql_connect($servername, $username, $password);
+mysql_select_db("sql6135475", $link);
 
-    $result = @mysql_query('SELECT * FROM reg where Name ="'.$name.'" ', $link);
-    $num_rows = @mysql_fetch_row($result);
-	echo $dbname = $num_rows[1];
-	echo $dbpass = $num_rows[2];
+$result = @mysql_query('SELECT * FROM reg where Name ="'.$name.'" ', $link);
+$num_rows = @mysql_fetch_row($result);
+echo $dbname = $num_rows[1];
+echo $dbpass = $num_rows[2];
 	
 ?>
