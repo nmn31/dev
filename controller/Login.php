0a9a1dfd70ca25ad0c1ad7a9dea60935655cdede
narender <kumar.narender927@gmail.com>
2016-09-13 10:30:22 +0530

    login

--- controller/Login.php
+++ controller/Login.php
@@ -5,9 +5,9 @@ if(isset($_POST['login'])||isset($_POST['name'])||isset($_POST['password']))
 	
 	        $name = $_POST['name'];	
 			$password= $_POST['password'];	
-	    if(!empty($name)&&!empty($password)){
-			include_once("../model/Loginmodel.php");
-			exit;
+	     if(!empty($name)&&!empty($password)){
+		 print_r($_POST);
+			/* include_once("../model/Loginmodel.php");
 		    if($name==$dbname&&$password==$dbpass){
 				session_start();
 				$_SESSION['user']= $name;
@@ -16,11 +16,11 @@ if(isset($_POST['login'])||isset($_POST['name'])||isset($_POST['password']))
 			}else{
 			    header("Location:https://".$_SERVER['SERVER_NAME'].'/index.php?id=notmached');
 				//header("location:http://localhost/login/index.php?id=notmached" );
-			}
+			} */
 	}else{
 	header("Location:https://".$_SERVER['SERVER_NAME'].'/index.php?id=enter');
 	//header("location:http://localhost/login/index.php?id=enter" );
-	}
+	} 
 	
 }
 
