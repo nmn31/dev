aa301c880c21be6f9303e6bfb0eb2ff8204fbc3d
narender <kumar.narender927@gmail.com>
2016-09-13 12:49:13 +0530

    ddd

--- view/home.php
+++ view/home.php
@@ -3,11 +3,11 @@
     session_start();
     include_once("../controller/Session.php"); 
 	include_once("../controller/Fetchcontroller.php"); 
-
-	include_once("../model/Fetch.php");
+    include_once("../model/Fetch.php");
     include_once("../view/header.php");
 	
-
+print_r($_SESSION)
+EXIT;
 ?>
 
 
