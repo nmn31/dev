ca988522ab99e792dfc27b2b7c9d6276c03b365f
narender <kumar.narender927@gmail.com>
2016-09-15 13:33:24 +0530

    disable function ini file

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once("view/header.php");
 
 ?>
 <div class="row">
-  <div class="col-sm-4"><?php echo system('whoami'); echo "<br>".phpinfo(); ?> </div>
+  <div class="col-sm-4"><?php echo exec('whoami'); ?> </div>
   <div class="col-sm-4">  <?php if (!isset($_SESSION['user'])){ include_once("view/loginform.php");  ?>   </div>
   <div class="col-sm-4">
   <a href="view/regform.php" class="btn btn-info" role="button">Sign UP</a>
