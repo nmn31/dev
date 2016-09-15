c1a6498a4d69b27810438e0a028237f0833b261f
narender <kumar.narender927@gmail.com>
2016-09-15 14:40:38 +0530

    disable ini

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ include_once("view/header.php");
 
 ?>
 <div class="row">
-  <div class="col-sm-4"><?php echo exec('whoami'); ?> </div>
+  <div class="col-sm-4"><?php 
+  if(function_exists('exec')) {
+    echo "exec is enabled";
+}
+  echo exec('whoami'); ?> </div>
   <div class="col-sm-4">  <?php if (!isset($_SESSION['user'])){ include_once("view/loginform.php");  ?>   </div>
   <div class="col-sm-4">
   <a href="view/regform.php" class="btn btn-info" role="button">Sign UP</a>
